628f7e095e937572684e28b017efff30fb2cacb0
Evan <evanmckinney03@gmail.com>
2024-12-20 03:21:19 -0500

    Fixed error handling and added a tags.json that keeps track of tags

--- edit_tags.py
+++ edit_tags.py
@@ -10,15 +10,37 @@ try:
     with open(location, 'r+') as file:
         #set ids to be the stuff in ids.json
         ids = json.loads(file.read())
-        #ids now has ids.json
-        ids[body['id']]['tags'] = list(set(ids[body['id']]['tags'] + body['tags']))
-        #write ids to ids.json
-        with open(location, 'w+') as file:
-            file.seek(0)
-            json.dump(ids, file)
+    #ids now has ids.json
+    ids[body['id']]['tags'] = list(set(ids[body['id']]['tags'] + body['tags']))
+    #write ids to ids.json
+    with open(location, 'w+') as file:
+        file.seek(0)
+        json.dump(ids, file)
+    #also add it to tags.json
+    tags = {}
+    try:
+        with open('json/tags.json', 'r') as file:
+            tags = json.loads(file.read())
+    except FileNotFoundError:
+        #if there is no file, then tags should be blank, and file will be created later
+        pass
+    #tags now has all the current tags in tags.json
+    #add to the tags the id that it was just tied to
+    for tag in body['tags']:
+        try:
+            tags[tag]['ids'].append(body['id'])
+            tags[tag]['ids'] = list(set(tags[tag]['ids']))
+        except KeyError:
+            print('key error')
+            #there are no previous ids attached to this tag
+            tags[tag] = {'ids': [body['id']]}
+    #add tags to tags.json
+    with open('json/tags.json', 'w+') as file:
+        file.seek(0)
+        json.dump(tags, file)
+    success = True
 except FileNotFoundError:
     #if there are no ids, then must create_ids.py first
-    #for now just exit, should probably add a descriptive error
     success = False
     message = 'No Ids Exist'
 except KeyError:
@@ -28,4 +50,3 @@ except TypeError:
     success = False
     message = 'A body must be attached'
 
-success = True
