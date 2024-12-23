364ee387bcf179cb93bc8448a113fac53f88424d
Evan <evanmckinney03@gmail.com>
2024-12-23 18:35:10 -0500

    Added edit_ids.py to edit existing attributes of ids

---
+++ edit_ids.py
@@ -0,0 +1,25 @@
+
+
+#try to open ids.json
+location = 'json/ids.json'
+try:
+    file = open(location, 'r+')
+    current_ids = json.load(file)
+    #attempt to edit current_ids
+    #assume body has an id and attr to edit
+    i = body['id']
+    for key in body:
+        if key != 'id' and key in current_ids[i]:
+            current_ids[i][key] = body[key]
+    #write back to ids.json
+    file.seek(0)
+    json.dump(current_ids, file)
+    file.truncate()
+    file.close()
+    success = True
+except FileNotFoundError:
+    success = False
+    message = 'No ids to edit'
+except KeyError:
+    success = False
+    message = 'Unable to edit given id'
