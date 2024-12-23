0c1fe6173da3967badcadfc8df0c770e1809f6c3
Evan <evanmckinney03@gmail.com>
2024-12-23 02:30:42 -0500

    Added a way to remove tags

--- remove_entry.py
+++ remove_entry.py
@@ -18,40 +18,69 @@ try:
     except:
         #there is a chance there are no tags but still trying to remove ids
         pass
+    try:
+        for i in ids_to_remove:
+            #see if there are any tags to remove
+            tags = current_ids[i]['tags']
+            #also need to remove the ids from tags.json
+            if len(tags) > 0 and current_tags is None:
+                success = False
+                message = 'Unable to open tags.json'
+                break
+            for tag in tags:
+                current_tags[tag]['ids'].remove(i)
+            del current_ids[i]
+        #write back to files
+        id_file.seek(0)
+        json.dump(current_ids, id_file)
+        id_file.truncate()
+        if tag_file is not None:
+            tag_file.seek(0)
+            json.dump(current_tags, tag_file)
+            tag_file.truncate()
+        #successfully removed ids
+        success = True
+    except KeyError:
+        #cannot find the id to remove
+        success = False
+        message = 'Unable to find the id to remove'
 
-    for i in ids_to_remove:
-        #see if there are any tags to remove
-        tags = current_ids[i]['tags']
-        #also need to remove the ids from tags.json
-        if len(tags) > 0 and current_tags is None:
-            success = False
-            message = 'Unable to open tags.json'
-            break
-        for tag in tags:
-            current_tags[tag]['ids'].remove(i)
-        del current_ids[i]
-    #write back to files
-    id_file.seek(0)
-    json.dump(current_ids, id_file)
-    id_file.truncate()
-    if tag_file is not None:
-       tag_file.seek(0)
-       json.dump(current_tags, tag_file)
-       tag_file.truncate()
-    #successfully removed ids
-    success = True
-    
 except KeyError:
     #there are no ids, try the tags next
     pass
 
 except (FileNotFoundError, json.JSONDecodeError):
     #cannot open the 'json/ids.json'
-    success = false
+    success = False
     message = 'There are no tags to remove'
 
 try:
     tags_to_remove = body['tags']
+    #open both ids and tags.json
+    id_file = open('json/ids.json', 'r+')
+    tag_file = open('json/tags.json', 'r+')
+    current_ids = json.load(id_file)
+    current_tags = json.load(tag_file)
+    try:
+        for t in tags_to_remove:
+            #see the ids that correspond to the tag
+            ids = current_tags[t]['ids']
+            for i in ids:
+                current_ids[i]['tags'].remove(t)
+            del current_tags[t]
+    except KeyError:
+        success = False
+        message = 'Cannot find tag to remove'
+    #write back to files
+    id_file.seek(0)
+    json.dump(current_ids, id_file)
+    id_file.truncate()
+    tag_file.seek(0)
+    json.dump(current_tags, tag_file)
+    tag_file.truncate()
+except (FileNotFoundError, json.JSONDecodeError):
+    success = False
+    message = 'Unable to open file'
 except KeyError:
     #there are no tags, so just move on
     pass
